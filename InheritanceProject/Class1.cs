3c37640a720d430a0ecdd3cbec3753b383396fb0
sameer singh <v-samesingh@microsoft.com>
2024-07-01 18:22:51 +0530

    Add project files.

---
+++ InheritanceProject/Class1.cs
@@ -0,0 +1,30 @@
+﻿namespace InheritanceProject
+{
+    class Class1
+    {
+        public Class1(int i)
+        {
+            System.Console.WriteLine("Constructor from Class1: "+i);
+        }
+       public void Test1()
+        {
+           System.Console.WriteLine("Method1 from Class1");
+        }
+      public void Test2()
+        {
+           System.Console.WriteLine("Method2 from Class1");
+        }
+
+        static void Main(string[] args)
+        {
+            Class1 p;    // p is a variable of Class1(no new memory allocation)
+            Class2 c = new Class2(); // c is a instance of Class2(new memory allocation)
+            p = c;      // p is a reference of parent class(Class1) created by using child class(Class2) instance
+            p.Test1();
+            p.Test2();
+            //Class1 p2 = new Class1();
+            //Console.WriteLine(p2.GetType());
+        }
+
+    }
+}
---
+++ InheritanceProject/Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProject
+{
+    
+     class Class2: Class1
+    {
+        public Class2():base(10)
+        {
+            System.Console.WriteLine("Constructor from Class2");
+        }
+        public void Test3()
+        {
+            System.Console.WriteLine("Method3 from Class2");
+        }
+        static void Main(string[] args)
+        {
+            Class2 obj = new Class2();
+            obj.Test1();
+            obj.Test2();
+            obj.Test3();
+        }
+    }
+}
